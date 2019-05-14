afefc332ffdb9583435a51933604247bb5a85170
FrancisFire <34342660+FrancisFire@users.noreply.github.com>
2019-05-14 16:15:36 +0200

    [UPDATE] Aggiunto parse opzioni

--- src/filestat.c
+++ src/filestat.c
@@ -2,36 +2,112 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
- 
+#include <getopt.h>
+int numeroMonitorati = 0;
+int numeroLink = 0;
+int numeroDirectory = 0;
+int dimensioneTotale = 0;
+int dimensioneMedia = 0;
+int dimensioneMassima = 0;
+int dimensioneMinima = 0;
+int verbose_flag;
+int stat_flag;
+int report_flag;
+int history_flag;
+int user_flag;
+int group_flag;
+int length_flag;
+int noscan_flag;
+
 int main(int argc, char **argv)
 {
-    if (argc != 2)    
+
+    static struct option long_options[] = {
+        {"verbose", no_argument, 0, 'v'},
+        {"stat", no_argument, 0, 's'},
+        {"report", no_argument, 0, 'r'},
+        {"history", required_argument, 0, 'h'},
+        {"user", required_argument, 0, 'u'},
+        {"group", required_argument, 0, 'g'},
+        {"length", required_argument, 0, 'l'},
+        {"noscan", no_argument, &noscan_flag, 1},
+        {0, 0, 0, 0}};
+    if (argc != 2)
         return 1;
- 
+    int c;
+
+    if (argc < 1)
+    {
+        return -1;
+    }
+
+    while (1)
+    {
+        int option_index = 0;
+        c = getopt_long(argc, argv, "vsrh:u:g:l:", long_options, &option_index);
+        if (c == -1)
+            break;
+        switch (c)
+        {
+        case 'v':
+            verbose_flag = 1;
+            printf("C'è V");
+            break;
+        case 's':
+            stat_flag = 1;
+
+            printf("C'è S");
+            break;
+        case 'r':
+            report_flag = 1;
+            printf("C'è R");
+            break;
+        case 'h':
+            user_flag = 1;
+            printf("C'è H");
+            //aggiungere gestione valori
+            break;
+        case 'u':
+            group_flag = 1;
+            printf("C'è U");
+            //aggiungere gestione valori
+            break;
+        case 'g':
+            length_flag = 1;
+            printf("C'è G");
+            //aggiungere gestione valori
+            break;
+        case 'l':
+            noscan_flag = 1;
+            printf("C'è L");
+            //aggiungere gestione valori
+            break;
+        }
+    }
     struct stat fileStat;
-    if(stat(argv[1],&fileStat) < 0)    
+    if (stat(argv[1], &fileStat) < 0)
         return 1;
- 
-    printf("Information for %s\n",argv[1]);
+
+    printf("Information for %s\n", argv[1]);
     printf("---------------------------\n");
-    printf("File Size: \t\t%d bytes\n",fileStat.st_size);
-    printf("Number of Links: \t%d\n",fileStat.st_nlink);
-    printf("File inode: \t\t%d\n",fileStat.st_ino);
- 
+    printf("File Size: \t\t%d bytes\n", fileStat.st_size);
+    printf("Number of Links: \t%d\n", fileStat.st_nlink);
+    printf("File inode: \t\t%d\n", fileStat.st_ino);
+
     printf("File Permissions: \t");
-    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
-    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
-    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
-    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+    printf((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+    printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    printf((fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    printf((fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    /* printf((fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    printf((fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    printf((fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
+    printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");*/
     printf("\n\n");
- 
-    printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
- 
+
+    // printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
+
     return 0;
 }
--- src/filestatvecchio.c
+++ src/filestatvecchio.c
@@ -5,80 +5,91 @@
 #include <sys/types.h>
 #include <string.h>
 #include <fcntl.h>
+#include <getopt.h>
 
-int numeroMonitorati = 0;
-int numeroLink = 0;
-int numeroDirectory = 0;
-int dimensioneTotale = 0;
-int dimensioneMedia = 0;
-int dimensioneMassima = 0;
-int dimensioneMinima = 0;
 
-void updateDimensioneMedia(){
-    dimensioneMedia = dimensioneTotale/(numeroDirectory+numeroLink+numeroMonitorati);
+
+
+
+void updateDimensioneMedia()
+{
+    dimensioneMedia = dimensioneTotale / (numeroDirectory + numeroLink + numeroMonitorati);
 }
 
-void updateDimensioneMassima(int newMax){
-    if(newMax > dimensioneMassima) dimensioneMassima = newMax;
+void updateDimensioneMassima(int newMax)
+{
+    if (newMax > dimensioneMassima)
+        dimensioneMassima = newMax;
 }
 
-void updateDimensioneMinima(int newMin){
-    if(newMin < dimensioneMinima) dimensioneMinima = newMin;
+void updateDimensioneMinima(int newMin)
+{
+    if (newMin < dimensioneMinima)
+        dimensioneMinima = newMin;
 }
 
-const char *get_filename_ext(const char *filename) {
+const char *get_filename_ext(const char *filename)
+{
     const char *dot = strrchr(filename, '.');
-    if(!dot || dot == filename) return "";
+    if (!dot || dot == filename)
+        return "";
     return dot + 1;
 }
 
-int index_option(int nrElem, char *options[], char *optionToSearch){
-    for(int i = 0; i<nrElem; i++){
-        if(strcmp(options[i], optionToSearch)){
+int index_option(int nrElem, char *options[], char *optionToSearch)
+{
+    for (int i = 0; i < nrElem; i++)
+    {
+        if (strcmp(options[i], optionToSearch))
+        {
             return i;
         }
     }
     return -1;
 }
 
-readPath(char *path, int isR, int isL){
+readPath(char *path, int isR, int isL)
+{
     struct stat *infoSingleFile;
-    if(l){
-        if(lstat(path, infoSingleFile) < 0) return -1;
-    } else {
-        if(stat(path, infoSingleFile) < 0) return -1;
+    if (isL)
+    {
+        if (lstat(path, infoSingleFile) < 0)
+            return -1;
     }
-    
+    else
+    {
+        if (stat(path, infoSingleFile) < 0)
+            return -1;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-    if(argc < 1){
-        return 1;
-    }
-
-    char const *options[argc - 3];
-    for(int i = 0; i< argc - 3 ; i++){
-        options[i] = argv[i+1];
-        printf(options[i]);
-    }
+    
 
     FILE *fileInput;
-    if(strcmp(get_filename_ext(argv[argc - 2]),"in")){
-      fileInput = fopen(argv[argc - 2], "r");
-    } else {
+    if (strcmp(get_filename_ext(argv[argc - 2]), "in"))
+    {
+        fileInput = fopen(argv[argc - 2], "r");
+    }
+    else
+    {
         fileInput = fopen("filestat.in", "r");
     }
 
     FILE *fileOutput;
-    if(strcmp(get_filename_ext(argv[argc - 1]),"db")){
-      fileOutput = fopen(argv[argc - 1], "a");
-    } else {
+    if (strcmp(get_filename_ext(argv[argc - 1]), "db"))
+    {
+        fileOutput = fopen(argv[argc - 1], "a");
+    }
+    else
+    {
         fileOutput = fopen("filestat.db", "a");
     }
 
     char *currentPath;
-    
-    fcloseall(void);
+
+    fclose(fileInput);
+    fclose(fileOutput);
     return 0;
 }
