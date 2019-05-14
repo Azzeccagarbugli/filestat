6ed271691a3c384d53866fd4d1c1ac3dbe372f78
FrancisFire <34342660+FrancisFire@users.noreply.github.com>
2019-05-14 17:14:14 +0200

    [UPDATE] IL PARSE DELLE OPZIONI FUNGE

--- src/filestat.c
+++ src/filestat.c
@@ -23,72 +23,72 @@ int main(int argc, char **argv)
 {
 
     static struct option long_options[] = {
-        {"verbose", no_argument, 0, 'v'},
-        {"stat", no_argument, 0, 's'},
-        {"report", no_argument, 0, 'r'},
+        {"verbose", no_argument, NULL, 'v'},
+        {"stat", no_argument, NULL, 's'},
+        {"report", no_argument, NULL, 'r'},
         {"history", required_argument, 0, 'h'},
         {"user", required_argument, 0, 'u'},
         {"group", required_argument, 0, 'g'},
         {"length", required_argument, 0, 'l'},
         {"noscan", no_argument, &noscan_flag, 1},
         {0, 0, 0, 0}};
-    if (argc != 2)
-        return 1;
-    int c;
+    int c = 0;
 
     if (argc < 1)
     {
         return -1;
     }
 
-    while (1)
+    int option_index = 0;
+    while (c != -1)
     {
-        int option_index = 0;
         c = getopt_long(argc, argv, "vsrh:u:g:l:", long_options, &option_index);
-        if (c == -1)
-            break;
         switch (c)
         {
         case 'v':
             verbose_flag = 1;
-            printf("C'è V");
             break;
         case 's':
             stat_flag = 1;
-
-            printf("C'è S");
             break;
         case 'r':
             report_flag = 1;
-            printf("C'è R");
             break;
         case 'h':
             user_flag = 1;
-            printf("C'è H");
             //aggiungere gestione valori
             break;
         case 'u':
             group_flag = 1;
-            printf("C'è U");
             //aggiungere gestione valori
             break;
         case 'g':
             length_flag = 1;
-            printf("C'è G");
             //aggiungere gestione valori
             break;
         case 'l':
             noscan_flag = 1;
-            printf("C'è L");
             //aggiungere gestione valori
             break;
         }
     }
-    struct stat fileStat;
-    if (stat(argv[1], &fileStat) < 0)
-        return 1;
 
-    printf("Information for %s\n", argv[1]);
+    if (verbose_flag)
+    {
+        printf("Verbose flag\n");
+    }
+    if (report_flag)
+    {
+        printf("Report flag\n");
+    }
+    if(stat_flag){
+        printf("Stat flag\n");
+    }
+    /* struct stat fileStat;
+    if (stat(argv[1], &fileStat) < 0)
+        return 1;*/
+
+    /*printf("Information for %s\n", argv[1]);
     printf("---------------------------\n");
     printf("File Size: \t\t%d bytes\n", fileStat.st_size);
     printf("Number of Links: \t%d\n", fileStat.st_nlink);
@@ -105,7 +105,7 @@ int main(int argc, char **argv)
     printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
     printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
     printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");*/
-    printf("\n\n");
+    //  printf("\n\n");
 
     // printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
 
