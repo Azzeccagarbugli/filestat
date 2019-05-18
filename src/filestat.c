7722242c3a2e2c4842e322f0406763385b8a6bab
azzeccagarbugli <f.coppola1998@gmail.com>
2019-05-18 10:09:40 +0200

    [UPDATE] Modifiche varie al file e sintassi migliorata

--- src/filestat.c
+++ src/filestat.c
@@ -1,8 +1,11 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <getopt.h>
+#include <ctype.h>
+
 int numeroMonitorati = 0;
 int numeroLink = 0;
 int numeroDirectory = 0;
@@ -10,6 +13,7 @@ int dimensioneTotale = 0;
 int dimensioneMedia = 0;
 int dimensioneMassima = 0;
 int dimensioneMinima = 0;
+
 int verbose_flag;
 int stat_flag;
 int report_flag;
@@ -19,19 +23,22 @@ int group_flag;
 int length_flag;
 int noscan_flag;
 
+char *min_max;
+
 int main(int argc, char **argv)
 {
     static struct option long_options[] = {
-        {"verbose", no_argument,        NULL,         'v'},
-        {"stat",    no_argument,        NULL,         's'},
-        {"report",  no_argument,        NULL,         'r'},
-        {"history", required_argument,  0,            'h'},
-        {"user",    required_argument,  0,            'u'},
-        {"group",   required_argument,  0,            'g'},
-        {"length",  required_argument,  0,            'l'},
-        {"noscan",  no_argument,        &noscan_flag,   1},
-        {0,         0,                  0,              0}
-        };
+        {"verbose", no_argument, NULL, 'v'},
+        {"stat", no_argument, NULL, 's'},
+        {"report", no_argument, NULL, 'r'},
+        {"history", required_argument, 0, 'h'},
+        {"user", required_argument, 0, 'u'},
+        {"group", required_argument, 0, 'g'},
+        {"length", required_argument, 0, 'l'},
+        {"noscan", no_argument, &noscan_flag, 1},
+        {0, 0, 0, 0}};
+
+    struct stat fileStat;
 
     int c = 0;
 
@@ -56,20 +63,20 @@ int main(int argc, char **argv)
             report_flag = 1;
             break;
         case 'h':
-            user_flag = 1;
-            //aggiungere gestione valori
+            history_flag = 1;
             break;
         case 'u':
-            group_flag = 1;
-            //aggiungere gestione valori
+            user_flag = 1;
             break;
         case 'g':
-            length_flag = 1;
-            //aggiungere gestione valori
+            group_flag = 1;
             break;
         case 'l':
+            length_flag = 1;
+            min_max = optarg;
+            break;
+        case 1:
             noscan_flag = 1;
-            //aggiungere gestione valori
             break;
         }
     }
@@ -82,33 +89,93 @@ int main(int argc, char **argv)
     {
         printf("Report flag\n");
     }
-    if(stat_flag){
+    if (stat_flag)
+    {
         printf("Stat flag\n");
     }
-    /* struct stat fileStat;
-    if (stat(argv[1], &fileStat) < 0)
-        return 1;*/
+    if (length_flag)
+    {
+        /*
+         * ./filestat -l <min>:<max> 
+         */
 
-    /*printf("Information for %s\n", argv[1]);
-    printf("---------------------------\n");
-    printf("File Size: \t\t%d bytes\n", fileStat.st_size);
-    printf("Number of Links: \t%d\n", fileStat.st_nlink);
-    printf("File inode: \t\t%d\n", fileStat.st_ino);
+        char *min_max_punt = min_max;
 
-    printf("File Permissions: \t");
-    printf((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
-    printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
-    printf((fileStat.st_mode & S_IWUSR) ? "w" : "-");
-    printf((fileStat.st_mode & S_IXUSR) ? "x" : "-");
-    /* printf((fileStat.st_mode & S_IRGRP) ? "r" : "-");
-    printf((fileStat.st_mode & S_IWGRP) ? "w" : "-");
-    printf((fileStat.st_mode & S_IXGRP) ? "x" : "-");
-    printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
-    printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
-    printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");*/
-    //  printf("\n\n");
- 
-    // printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
+        int index = 0;
+
+        //char * p = strtok(min_max_punt, ":");
+
+        long values[2];
+
+        while (*min_max_punt)
+        {
+            if (isdigit(*min_max_punt))
+            {
+                long val = strtol(min_max_punt, &min_max_punt, 10);
+                values[index++] = val;
+            }
+            else
+            {
+                min_max_punt++;
+            }
+        }
+
+        /*
+        if (values[0] == 0 && values[1] == 0)
+        {
+            printf("The inserted values are not valid\n");
+            return 1;
+        }
+        else if (values[0] == 0)
+        {
+            printf("Si vogliono cercare i file minori di: %ld", values[1]);
+        }
+        else if (values[1] == 0)
+        {
+            printf("Si volgiono cercare i file maggiori di: %ld", values[0]);
+        }
+        else if (values[0] > values[1])
+        {
+            printf("The first value must be major than the second one\n");
+            return 1;
+        }
+        */
+
+        for (index = 0; index < sizeof(values) / sizeof(values[0]); index++)
+        {
+            printf("%i - %ld\n", index, values[index]);
+        }
+    }
+    if (noscan_flag)
+    {
+        if (stat(argv[2], &fileStat) < 0)
+        {
+            return 1;
+        }
+
+        // print_no_scan(argv[2], fileStat);
+
+        printf("Information for %s\n", argv[2]);
+        printf("---------------------------\n");
+        printf("File Size: \t\t%d bytes\n", fileStat.st_size);
+        printf("Number of Links: \t%d\n", fileStat.st_nlink);
+        printf("File inode: \t\t%d\n", fileStat.st_ino);
+
+        printf("File Permissions: \t");
+        printf((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+        printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
+        printf((fileStat.st_mode & S_IWUSR) ? "w" : "-");
+        printf((fileStat.st_mode & S_IXUSR) ? "x" : "-");
+        printf((fileStat.st_mode & S_IRGRP) ? "r" : "-");
+        printf((fileStat.st_mode & S_IWGRP) ? "w" : "-");
+        printf((fileStat.st_mode & S_IXGRP) ? "x" : "-");
+        printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
+        printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
+        printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");
+        printf("\n\n");
+
+        printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
+    }
 
     return 0;
 }
